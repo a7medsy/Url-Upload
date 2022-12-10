1bf7df8e1075fa0ae4a125cd62ca3dad9170b944
a7medsy <103423794+a7medsy@users.noreply.github.com>
2022-12-10 03:29:24 +0300

    Add files via upload

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -46,11 +46,11 @@ async def start(bot, update):
             [
                 [
                     InlineKeyboardButton(
-                        "Source code ⚡", url="https://github.com/Clinton-Abraham/UPLOADER-BOT"
+                        "Source code ⚡", url="https://t.me/+uPg3TPNFuckwMDU0"
                     ),
-                    InlineKeyboardButton("Project Channel 👨🏻‍💻", url="https://t.me/Space_X_bots"),
+                    InlineKeyboardButton("Project Channel 👨🏻‍💻", url="https://t.me/+uPg3TPNFuckwMDU0"),
                 ],
-                [InlineKeyboardButton("Developer 👨‍⚖️", url="https://t.me/clinton_abraham")],
+                [InlineKeyboardButton("Developer 👨‍⚖️", url="https://t.me/A7_SYR")],
             ]
         ),
         reply_to_message_id=update.message_id
